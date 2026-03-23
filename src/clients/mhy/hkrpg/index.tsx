bc2c54631bed9f68e6f6885139ce08f126b2a557
riverfog7 <cjw9770@gmail.com>
2026-03-24 01:00:32 +0900

    Allow all game versions up to 9.9.9

--- src/clients/mhy/hkrpg/index.tsx
+++ src/clients/mhy/hkrpg/index.tsx
@@ -44,7 +44,7 @@ import createPatchOff from "./config/patch-off";
 import createBlockNet from "./config/block-net";
 import { getLatestAdvInfo, getLatestVersionInfo } from "../hyp-connect";
 
-const CURRENT_SUPPORTED_VERSION = "4.0.0";
+const CURRENT_SUPPORTED_VERSION = "9.9.9";
 
 export async function createHKRPGChannelClient({
   server,
